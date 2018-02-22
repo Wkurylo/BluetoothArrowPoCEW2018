a8a138d65c4a38c8112cff2a5e84b68ece89b886
wojtekkurylo <wkurylo88@gmail.com>
2018-02-22 15:52:42 +0100

    Initial commit

---
+++ Application/src/main/java/com/example/android/bluetoothlegatt/StarterPermissionChecker.java
@@ -0,0 +1,69 @@
+package com.example.android.bluetoothlegatt;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Handler;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
+
+public class StarterPermissionChecker extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starter_permission_checker);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                checkPermissions();
+            }
+        }, 3500);
+
+    }
+
+    public void checkPermissions() {
+
+        // Documentation: https://github.com/Karumi/Dexter
+        Dexter.withActivity(this)
+                .withPermissions(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.BLUETOOTH_ADMIN,
+                        Manifest.permission.BLUETOOTH
+                ).withListener(new MultiplePermissionsListener() {
+            @Override
+            public void onPermissionsChecked(MultiplePermissionsReport report) {
+                if (report.areAllPermissionsGranted()) {
+
+                    Intent intent = new Intent(StarterPermissionChecker.this, DeviceScanActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                token.continuePermissionRequest();
+            }
+
+        }).check();
+
+    }
+
+}
